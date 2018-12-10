8c05d97a96a55aed8320e2c183afea03ae16c090
Eduard Predescu <eduard.florin.predescu@gmail.com>
2018-12-10 11:32:24 +0000

    changed getter so it does not error

--- src/store/user/getters.js
+++ src/store/user/getters.js
@@ -1,3 +1,3 @@
 export const isAuthenticated = state => !!state.token;
-export const filteredUsers = state =>
-  state.users.filter(user => user._id !== state.currentUser._id);
+export const filteredUsers = state => state.currentUser ?
+  state.users.filter(user => user._id !== state.currentUser._id) : [];
